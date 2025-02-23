908cc2fc6d4c2559038d6ff32dae138872f03674
Saintmoriel <nimrodyusuf@gmail.com>
2025-02-23 20:37:31 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { ExpoRoot } from "expo-router";
+import { registerRootComponent } from "expo";
+
+export default function App() {
+    return <ExpoRoot context={require.context("./app")} />;
+}
+
+registerRootComponent(App);
