88cca816dc3656ad7c2bae46a15eee626739481d
Saintmoriel <nimrodyusuf@gmail.com>
2025-02-25 17:10:00 +0100

    feat: Configure status bar, app icon and splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+
 export default function Layout() {
     return (
+      <>
     <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="index" options={{ title: 'Home' }} />
         <Stack.Screen name="about" options={{ title: 'About' }} />
     </Stack>
+    <StatusBar style="light" />
+    </>
     );
 }
 /*
